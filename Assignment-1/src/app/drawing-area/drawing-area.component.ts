9e4b1ce299768fa627fdaa7ee00ac4e8893675d7
Milyn <milyng.dsilva@gmail.com>
2022-07-25 13:14:08 +0530

    reverted temporary changes

--- Assignment-1/src/app/drawing-area/drawing-area.component.ts
+++ Assignment-1/src/app/drawing-area/drawing-area.component.ts
@@ -6,22 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drawing-area.component.css']
 })
 export class DrawingAreaComponent implements OnInit {
- // public color:any
-  colorOne = 'base';
-  public classes :any;
+  //public color:any
+  classes = 'base';
   constructor() { }
 
-  ngOnInit() {
-    //this.colorOne = ''
-    //this.changeColor('base')
+  ngOnInit(){
+   //this.changeColor('base')
   }
 
-  changeColor(color: string) {
-    this.colorOne = color
+  changeColor(color:string){
     //this.color = color
     this.classes = color //does not work
-    // const doc = document.getElementById('color')
-    // doc!.style.backgroundColor = color
-    console.log("Color change was called from drawing area component", color)
+    const doc = document.getElementById('color')
+    doc!.style.backgroundColor = color
+    console.log("Color change was called from drawing area component",color)    
   }
 }
